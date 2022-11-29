8b67c2bfe4775e5e4fadc0d45e232cfc444d6710
buzov <abuzov@list.ru>
2022-11-29 17:50:01 +0300

    all functions created

--- utils.py
+++ utils.py
@@ -12,3 +12,18 @@ def get_all(candidates):
         print(candidate['position'])
         print(candidate['skills'])
         print()
+
+
+def get_by_pk(pk, candidates):
+    for candidate in candidates:
+        if candidate['pk'] == pk:
+            return candidate
+    return
+
+
+def get_by_skill(skill_name, candidates):
+    result = []
+    for candidate in candidates:
+        if skill_name.lower() in candidate.lower().split(', '):
+            result.append(candidate)
+    return result
